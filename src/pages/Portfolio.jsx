06d74a37e36c67fff3d28bc8e5507d866db59ec8
sahrios <sahriosrodrigues@gmail.com>
2022-12-08 23:22:54 -0300

    adicionando alguns links

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,7 +1,8 @@
 import { Header } from "../components/Header";
 import portfolioImg from "../assets/portfolio.svg";
 import styles from "../styles/pages/portfolio.module.css";
-
+import projetofinalImg from "../assets/projeto-final-pretalab.png"
+import nlwImg from "../assets/nlw.png"
 
 
 export function Portfolio() {
@@ -9,18 +10,23 @@ export function Portfolio() {
       <>
         <Header text="Meus projetos" image={portfolioImg} />
 
-        <div className={styles.homeContainer}>
-
-        <div className={styles.cardContainer}>
-          <h2 className={styles.cardTitle}>Meus Projetos</h2>
-        </div>
-
-        </div>
-
-        <div className={styles.cardContainer}>
+          <h2 className={styles.cardTitle}> Meus Projetos</h2>
+          
+          <div>
           <h2 className={styles.cardTitle}>Meu Portfólio</h2>
-          <p className={styles.cardText}>Mostro meu portfólio. Tudo o que aprendi até o momento dando destaque para alguns dos projetos que criei enquanto estudo programação.</p>
-        </div>
+          <img className={styles.image} src={projetofinalImg} />
+          <p className={styles.cardText}>Um site em react para demonstrar meu portfólio.</p>
+          <a className={styles.projectButton} href= "https://meu-projeto-final-pretalab.vercel.app/" target= "_blank">Ver projeto</a>
+          </div>
+
+          <div>
+          <h2 className={styles.cardTitle}>NLW eSports</h2>
+          <img className={styles.image} src={nlwImg} />
+          <p className={styles.cardText}>Projeto construído no evento Next Level Week da Rocketseat.</p>
+          <a className={styles.projectButton} href= "https://nextlevelweek.netlify.app/" target= "_blank">Ver projeto</a>
+          </div>
+
+
 
 
 
